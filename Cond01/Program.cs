8669467dfa53f098aa2e9f32a5122a539dad52c0
Hiury Gomes <hiury_jp@hotmail.com>
2021-10-01 13:50:30 -0300

    Cond 01 if else prof Nelio

---
+++ Cond01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace cond01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int N = int.Parse(Console.ReadLine());
+
+            if (N < 0)
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("NAO NEGATIVO");
+            }
+        }
+    }
+}
